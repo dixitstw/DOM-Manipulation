7af33009631553690a068164314b94d2b895b9e7
Dixit <dixitgautam01@gmail.com>
2022-08-14 19:51:48 +0545

    First commit

---
+++ app.js
@@ -0,0 +1,54 @@
+//const title = document.getElementById('main-heading');
+//console.log(title);
+
+//const listItem = document.getElementsByClassName('list-items');
+//console.log(listItem);
+
+//const listItems = document.getElementsByTagName('li');
+//console.log(listItems);
+
+//const container = document.querySelector('div');
+//console.log(container);
+
+//const container = document.querySelectorAll('div');
+//console.log(container);
+
+//const title = document.querySelector('#main-heading');
+//console.log(title);
+
+//title.style.color = 'red';
+
+//const listItems = document.querySelectorAll('.list-items');
+//console.log(listItems);
+
+
+//for(i=0; i < listItems.length; i++) {
+  //   listItems[i].style.fontSize = '5rem';
+//}
+
+//Creating Elements
+
+//const ul = document.querySelector('ul');
+//const li = document.createElement('li');
+
+//console.log(li);
+
+//Adding Elements
+
+//ul.append(li)
+
+//Modifying the text
+//li.innertext = 'X-men';
+
+//Modifying Attributes and Classes
+
+//li.setAttribute('id', 'main-heading');
+//li.removeAttribute('id');
+
+//li.classList.add('list-items');
+//console.log(li.classList.contains('list-items'));
+
+//li.remove();
+
+
+
